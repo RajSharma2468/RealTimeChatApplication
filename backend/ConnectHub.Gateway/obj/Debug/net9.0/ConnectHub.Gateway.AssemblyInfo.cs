c17f63e0ef1bfb48550a924ebb71468c5bca860a
raj <rajsharmakosi23@gmail.com>
2026-04-18 05:52:15 +0530

    Deploy Auth Service to Azure

--- backend/ConnectHub.Gateway/obj/Debug/net9.0/ConnectHub.Gateway.AssemblyInfo.cs
+++ backend/ConnectHub.Gateway/obj/Debug/net9.0/ConnectHub.Gateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectHub.Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14ef25c4906e88c68855f2b566a25eebeda8525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cce2b48490a64f68f0026000b970be00184f838")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectHub.Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectHub.Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/ConnectHub.Presence/obj/Debug/net9.0/ConnectHub.Presence.AssemblyInfo.cs
+++ backend/ConnectHub.Presence/obj/Debug/net9.0/ConnectHub.Presence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectHub.Presence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14ef25c4906e88c68855f2b566a25eebeda8525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cce2b48490a64f68f0026000b970be00184f838")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectHub.Presence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectHub.Presence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
